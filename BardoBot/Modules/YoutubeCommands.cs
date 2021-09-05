d795bdd04a9a808165ddd1e66a9afa2ed82a2776
helderdev <hdrhelder5@gmail.com>
2021-09-05 19:02:10 -0300

    first commit

---
+++ BardoBot/Modules/YoutubeCommands.cs
@@ -0,0 +1,92 @@
+﻿using BardoBot.Services;
+using CliWrap;
+using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using Discord.WebSocket;
+using SharpLink;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+namespace BardoBot.Modules
+{
+    public class YoutubeCommands : ModuleBase
+    {
+
+         private async Task<Stream> DownloadVideo(string url)
+        {
+            ///TODO: APAGAR ARQUIVOS
+            ///
+
+            var file = Directory.GetFiles(@"D:\Repos\BardoBot\BardoBot\bin\Debug\netcoreapp3.1").Where(f => f.Contains(".mp4"));
+            File.Delete(@"D:\Repos\BardoBot\BardoBot\bin\Debug\netcoreapp3.1\video.mp4");
+            var youtube = new YoutubeClient();
+
+            // You can specify both video ID or URL
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+
+            // ...or highest bitrate audio-only stream
+            var streamInfo = streamManifest.GetAudioOnlyStreams()
+                 .Where(s => s.Container == Container.Mp4)
+                .GetWithHighestBitrate();
+            var stream = youtube.Videos.Streams.GetAsync(streamInfo);
+
+            //await youtube.Videos.Streams.DownloadAsync(streamInfo, $"video.{streamInfo.Container}");
+
+            return await youtube.Videos.Streams.GetAsync(streamInfo);
+
+        }
+
+        [Command("join")]
+        public async Task Join(IAudioChannel channel = null) //SocketUser user, SocketVoiceState state1, SocketVoiceState state2)
+        {
+
+            //  IAudioClient audioClient = await (Context.User as IVoiceState).VoiceChannel.ConnectAsync();
+
+            var UserCheck = Context.Guild.GetUserAsync(Context.User.Id);
+            channel ??= (Context.Message.Author as IGuildUser)?.VoiceChannel;
+            var roleCheck = Context.Guild.Roles.FirstOrDefault(x => x.Name == "DJ");
+
+            //if (UserCheck.Result.RoleIds.ToList().Contains(roleCheck.Id))
+            //{
+                if (channel == null)
+                {
+                    await Context.Message.DeleteAsync();
+                    await ReplyAsync("You need to be in a voice channel, or pass one as an argument.");
+                    return;
+                }
+                else
+                {
+
+                IAudioClient audioClient = await channel.ConnectAsync();
+                //IAudioClient audioClient = channel.ConnectAsync().Result;
+
+
+
+            }
+            //}
+            //else
+            //{
+            //    await Context.Message.DeleteAsync();
+            //    await ReplyAsync("Insufficient Permissions");
+            //}
+        }
+
+
+        [Command("play", RunMode = RunMode.Async)]
+        public async Task PlayCmd(string song, IAudioChannel channel = null)
+        {
+            if (channel == null)
+            {
+                channel = (Context.User as IVoiceState).VoiceChannel;
+            }
+            var stream = await DownloadVideo(song);
+            var service = new AudioHandler();
+            await service.SendAudioAsync(Context.Guild, channel, Context.Channel, stream);
+        }
+
+  
+    }
+}
---
+++ BardoBot/Services/AudioHandler.cs
@@ -0,0 +1,86 @@
+﻿using Discord;
+using Discord.Audio;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BardoBot.Services
+{
+    public class AudioHandler
+    {
+        private readonly ConcurrentDictionary<ulong, IAudioClient> ConnectedChannels = new ConcurrentDictionary<ulong, IAudioClient>();
+
+        public async Task JoinAudio(IGuild guild, IVoiceChannel target)
+        {
+            IAudioClient client;
+            if (ConnectedChannels.TryGetValue(guild.Id, out client))
+            {
+                return;
+            }
+            if (target.Guild.Id != guild.Id)
+            {
+                return;
+            }
+
+            var audioClient = await target.ConnectAsync();
+
+            if (ConnectedChannels.TryAdd(guild.Id, audioClient))
+            {
+                // If you add a method to log happenings from this service,
+                // you can uncomment these commented lines to make use of that.
+                //await Log(LogSeverity.Info, $"Connected to voice on {guild.Name}.");
+            }
+        }
+
+        public async Task LeaveAudio(IGuild guild)
+        {
+            IAudioClient client;
+            if (ConnectedChannels.TryRemove(guild.Id, out client))
+            {
+                await client.StopAsync();
+                //await Log(LogSeverity.Info, $"Disconnected from voice on {guild.Name}.");
+            }
+        }
+
+        public async Task SendAudioAsync(IGuild guild, IAudioChannel channel, IMessageChannel txtChannel, Stream streamFlush)
+        {
+            string path = @"https://www.youtube.com/watch?v=Zh5bW-TuA7U";
+            // Your task: Get a full path to the file if the value of 'path' is only a filename.
+            //if (!File.Exists(path))
+            //{
+            //    await txtChannel.SendMessageAsync("File does not exist.");
+            //    return;
+            //}
+             var client = await channel.ConnectAsync();
+
+            //IAudioClient client = ;
+            //if (ConnectedChannels.TryGetValue(guild.Id, out client))
+            //{
+            //await Log(LogSeverity.Debug, $"Starting playback of {path} in {guild.Name}");
+
+
+            using (var ffmpeg = CreateProcess(path))
+            using (var stream = client.CreatePCMStream(AudioApplication.Music))
+            {
+                try { await ffmpeg.StandardOutput.BaseStream.CopyToAsync(stream); }
+                finally { await stream.FlushAsync(); }
+            }
+            //}
+        }
+
+        private Process CreateProcess(string path)
+        {
+            return Process.Start(new ProcessStartInfo
+            {
+                FileName = @"C:\Users\Helder\Downloads\ffmpeg-2021-09-05-git-a947098558-full_build\bin\ffmpeg.exe",
+                Arguments = $"-i {path} -c copy output.mp3",
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            });
+        }
+    }
+}
